ef4595e2ee598762f44cf289df43eeead2d8eb91
anandishra <141013172+anandishra@users.noreply.github.com>
2023-07-31 13:44:27 +0530

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,11 @@
+<?php
+    include "connection.php";
+    if(isset($_GET['id']))
+    {
+        $id = $_GET['id'];
+        $sql = "DELETE from `crud2` where id=$id";
+        $conn->query($sql);
+    }
+    header('location:/CURD100/index.php');
+    exit;
+?>
